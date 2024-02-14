d5b2900e9717d5a34573435cb09657f557820daa
Nguyen Ha <nguyenhbhcm@gmail.com>
2024-02-13 22:15:26 -0800

    Action after uploading resume

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,16 @@
 import streamlit as st
+from streamlit_tags import st_tags
 from pages import job_recommendation
+from pathlib import Path
+import os
+import yaml
+
+config = yaml.safe_load(open("YOUR_CONFIG_FILE.yml"))
+for key, value in config.items():
+    if isinstance(value, str):
+        config[key] = Path(value)
+        if 'dir' in key:
+            config[key].mkdir(parents=True, exist_ok=True)
 
 def app():
     # Set page title and icon
@@ -17,8 +28,25 @@ def app():
     if selection == 'Job Recommendation':
         job_recommendation.app()
 
-    uploaded_file = st.file_uploader("Upload your resume", type="pdf")
-    if uploaded_file is not None:
-        pass
+    uploaded_file = st.file_uploader(label="Upload your resume", type="pdf")
+    if uploaded_file:
+        save_path = Path(config['pdf_dir'], uploaded_file.name)
+        with open(save_path, mode='wb') as w:
+            w.write(uploaded_file.getvalue())
+
+        # TODO: Analyze the PDF to extract skills
+        skills = ['Python', 'SQL', 'Machine Learning', 'Data Analysis']
+
+        if skills:
+            keywords = st_tags(
+                            label='### Skills:',
+                            text='Press enter to add more',
+                            value=skills,
+                            )
+            
+        if st.button('See which jobs are a good fit for you'):
+            job_recommendation.app()
+
+        
 
 app()
