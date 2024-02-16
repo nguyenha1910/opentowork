a5ccd6018d20cb4ba86073027c66a959af13935d
ymkim814 <78923564+ymkim814@users.noreply.github.com>
2024-02-15 17:43:03 -0800

    Update home.py
    Add extraction stuff in the right file

--- pages/home.py
+++ pages/home.py
@@ -4,6 +4,15 @@ from pages import job_recommendation
 from pathlib import Path
 import os
 import yaml
+import PyPDF4
+import fitz  # PyMuPDF library
+#from sentence_transformers import (SentenceTransformer, util)
+#import numpy as np
+#from numpy.linalg import norm
+#from statistics import mean
+import spacy
+import pandas as pd
+import sys
 
 config = yaml.safe_load(open("YOUR_CONFIG_FILE.yml"))
 for key, value in config.items():
@@ -35,7 +44,7 @@ def app():
             w.write(uploaded_file.getvalue())
 
         # TODO: Analyze the PDF to extract skills
-        skills = ['Python', 'SQL', 'Machine Learning', 'Data Analysis']
+        skills = skill_extraction(save_path)
 
         if skills:
             keywords = st_tags(
@@ -47,6 +56,38 @@ def app():
         if st.button('See which jobs are a good fit for you'):
             job_recommendation.app()
 
-        
+def skill_extraction(path):
+    with fitz.open(path) as pdf_resume:
+        extracted_resume_content_PyMuPDF = ""
+        for page_number in range(pdf_resume.page_count):
+            page = pdf_resume[page_number]
+            extracted_resume_content_PyMuPDF += page.get_text()
+
+    # print statement that checks the content
+    #print("extracted_resume_content_PyPDF4:", extracted_resume_content_PyMuPDF)
+
+    # needs to have this file downloaded
+    skills = "jz_skill_patterns.jsonl" 
+    # https://medium.com/@vikrantptl06/resume-parsing-using-spacy-af24376ec008
+    # https://github.com/kingabzpro/jobzilla_ai/blob/main/jz_skill_patterns.jsonl
+
+    ruler = nlp.add_pipe("entity_ruler", before = "ner")
+    ruler.from_disk(skills)
+    doc = nlp(extracted_resume_content_PyMuPDF)
+
+    skills = [ent.text for ent in doc.ents if ent.label_ == "SKILL"]
+
+    dict = {}
+    skills = []
+
+    for ent in doc.ents:
+        #print(ent.label_)
+        if "SKILL" in ent.label_:
+            skills.append(ent.text)
+
+    # remove duplicates (capitalization)
+    lowercase_skills = [s.lower() for s in skills]
+    unique_skills = list(set(lowercase_skills))
+    return unique_skills
 
 app()
