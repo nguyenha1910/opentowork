d32010e2d70438102f2c9cc04041454ed45f428a
Nguyen Ha <nguyenhbhcm@gmail.com>
2024-02-29 20:50:10 -0800

    fix null job description

--- pages/job_recommendation.py
+++ pages/job_recommendation.py
@@ -1,32 +1,28 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import yaml
+import os
 from opentowork import sim_calculator
-from opentowork import skill_extraction
-
-config = yaml.safe_load(open("config.yml"))
-# Need to check with different file like job_listings_Data Scientist.csv
-job_posting= pd.read_csv("job_listings_data analyst_10_pages.csv")
-
-@st.cache_data
-def load_data(path):
-    df = pd.read_csv(path)
-    return df
 
 def job_item(data, skills):
-    score = sim_calculator(data, skills)
+    # score = data['score']
+    score = sim_calculator(data['description'], skills)
     container = st.container(border=True)
     c1, c2 = container.columns([5, 1])
     c1.subheader(data['title'])
     c1.write(data['company'])
     c1.caption(data['location'])
     c2.link_button("Apply", data['link'])
-    c2.progress(score, text=f"{score}%")
+    c2.progress(score, text=f"{int(score*100)}%")
     return container
 
 def app(skills):
-    data = load_data(config['data_path'])
 
-    for id, row in data.iterrows():
-        job_item(row, skills)
+    data_path = "csvs/job_listings_['data analyst', 'data scientist', 'data engineer']_1_pages.csv"
+    data = pd.read_csv(data_path)
+
+    for _, row in data.iterrows():
+        if not pd.isna(row['description']):
+            job_item(row, skills)
+        else:
+            continue
+            
