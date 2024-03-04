4b16f034b74e9e88f0fa73c1bc5426230a2e8d10
Nguyen Ha <nguyenhbhcm@gmail.com>
2024-03-04 14:48:24 -0800

    fix hardcoded skill pattern file

--- opentowork/skill_extraction.py
+++ opentowork/skill_extraction.py
@@ -5,6 +5,10 @@ spacy module for entity assignment and extraction of skill set from a user's res
 import fitz  # PyMuPDF library
 import spacy
 import os
+import yaml
+
+with open("config.yml", "r") as config_file:
+    config = yaml.safe_load(config_file)
 
 nlp = spacy.load("en_core_web_lg") # python -m spacy download en_core_web_lg
 
@@ -28,44 +32,48 @@ def skill_extraction_resume(path):
             page = pdf_resume[page_number]
             extracted_resume_content_PyMuPDF += page.get_text()
 
-    # need to have this file downloaded
-    skills = "raw/jz_skill_patterns.jsonl"
-    # https://github.com/kingabzpro/jobzilla_ai/blob/main/jz_skill_patterns.jsonl
+    skill_patterns_path = config['pattern_path']
+    if not os.path.isfile(skill_patterns_path):
+        link = "https://github.com/kingabzpro/jobzilla_ai/blob/main/jz_skill_patterns.jsonl"
+        raise FileNotFoundError(f"{skill_patterns_path} not found.\nPlease download the file from {link}")
 
     if "entity_ruler" not in nlp.pipe_names:
         ruler = nlp.add_pipe("entity_ruler", before = "ner")
-        ruler.from_disk(skills)
+        ruler.from_disk(skill_patterns_path)
     doc = nlp(extracted_resume_content_PyMuPDF)
-    skills = [ent.text for ent in doc.ents if ent.label_ == "SKILL"]
+    skill_patterns_path = [ent.text for ent in doc.ents if ent.label_ == "SKILL"]
 
     for ent in doc.ents:
         if "SKILL" in ent.label_:
-            skills.append(ent.text)
+            skill_patterns_path.append(ent.text)
 
     # remove duplicates (capitalization)
-    lowercase_skills = [s.lower() for s in skills]
+    lowercase_skills = [s.lower() for s in skill_patterns_path]
     unique_skills = list(set(lowercase_skills))
     return unique_skills
 
 def skill_extraction_job_description(description_row):
-    # need to have this file downloaded
-    skills = "raw/jz_skill_patterns.jsonl"
-    # https://github.com/kingabzpro/jobzilla_ai/blob/main/jz_skill_patterns.jsonl
+    """
+    Extract the skill set from the job description  
+    Args:
+        description_row (string): job description
+    Returns:
+        list of string : returns the list of string consists of unique skils
+    """
+
+    skill_patterns_path = config['pattern_path']
+    if not os.path.isfile(skill_patterns_path):
+        link = "https://github.com/kingabzpro/jobzilla_ai/blob/main/jz_skill_patterns.jsonl"
+        raise FileNotFoundError(f"{skill_patterns_path} not found.\nPlease download the file from {link}")
 
     if "entity_ruler" not in nlp.pipe_names:
         ruler = nlp.add_pipe("entity_ruler", before = "ner")
-        ruler.from_disk(skills)
+        ruler.from_disk(skill_patterns_path)
 
     doc = nlp(description_row)
-    skills = [ent.text for ent in doc.ents if ent.label_ == "SKILL"]
-
-    dict = {}
-    skills = []
-    for ent in doc.ents:
-        if "SKILL" in ent.label_:
-            skills.append(ent.text)
+    skill_patterns_path = [ent.text for ent in doc.ents if "SKILL" in ent.label_]
 
     # remove duplicates (capitalization)
-    lowercase_skills = [s.lower() for s in skills]
+    lowercase_skills = [s.lower() for s in skill_patterns_path]
     unique_skills = list(set(lowercase_skills))
     return unique_skills
