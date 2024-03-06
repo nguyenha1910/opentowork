f931c7f00809a6d0e4473ca88c81f05dd557bb15
Kelly <zwang28@uw.edu>
2024-03-05 16:15:10 -0800

    fix sim_calculator

--- opentowork/sim_score_calculator_new.py
+++ opentowork/sim_score_calculator_new.py
@@ -4,7 +4,7 @@ utilizes pytorch for cosine similairty calculation.
 """
 from sentence_transformers import SentenceTransformer, util
 
-def sim_calculator(job_posting_skills, resume_skills):
+def sim_calculator(job_posting, resume):
     """
     A function that calculates the similarity score between vector embedding of
     job description and skill set extracted from the user's resume based on the cosine similairty.
@@ -12,14 +12,16 @@ def sim_calculator(job_posting_skills, resume_skills):
 
     Args:
         job_posting (_string_): the text of job posting scraped
-        skill_set (_list of string_): the list of skill set extracted from the resume
+        resume (_string_): the text of resume content 
+        #skill_set (_list of string_): the list of skill set extracted from the resume
 
     Returns:
         float : absolute value of similarity score calculated between two inputs
     """
-
+    #print("job_posting:", job_posting)
+    #print("resume", resume)
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L12-v2")
-    job_embedding = model.encode(job_posting_skills)
-    skill_embedding = model.encode(resume_skills)
-    similarity_torch = util.pytorch_cos_sim(skill_embedding, job_embedding).tolist()[0][0]
+    job_embedding = model.encode([job_posting])
+    resume_embedding = model.encode([resume])
+    similarity_torch = util.pytorch_cos_sim(resume_embedding, job_embedding).tolist()[0][0]
     return abs(similarity_torch)
