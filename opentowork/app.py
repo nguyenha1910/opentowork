d5293543eae78a9ec498dbdd31a88e962dd1c99f
Elaine Zhang <ezhang33@uw.edu>
2024-03-07 15:37:09 -0800

    update subprocess command

--- opentowork/app.py
+++ opentowork/app.py
@@ -47,7 +47,7 @@ def app():
         )
 
         if st.button('Update Job Posting Data'):
-            subprocess.run(["python", "-m", "opentowork.scraper.job_listing_scraper"],check=True)
+            subprocess.run(["python", "-m", "scraper.job_listing_scraper"],check=True)
 
         job_recommendation.app(skills_resume, resume_content)
 
