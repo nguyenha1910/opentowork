0da30ed1d5cf694cb3b79df5cb31f71a0d89fd9c
Nguyen Ha <nguyenhbhcm@gmail.com>
2024-03-07 20:05:19 -0800

    pylint check

--- opentowork/pages/job_recommendation.py
+++ opentowork/pages/job_recommendation.py
@@ -1,14 +1,16 @@
 # pylint: disable=import-error
+# pylint: disable=too-many-arguments
 # pylint runs from a different place than deployed app
 """
 This module represents the job list of the app.
 """
 import os
 from pathlib import Path
+from datetime import datetime
 import streamlit as st
 import pandas as pd
-from skill_extraction import get_job_description_skills #opentowork.
-from sim_score import get_sim_score #opentowork.
+from skill_extraction import get_job_description_skills
+from sim_score import get_sim_score
 
 def get_latest_csv_file():
     """
@@ -58,7 +60,7 @@ def job_item(data, skills_jd, skills_resume, jd_content, resume_content, key):
               skills are present in your resume.")
     return container
 
-def status_update(data): 
+def status_update(data):
     """
     The main app that creates and updates job application
     Args:
@@ -67,14 +69,22 @@ def status_update(data):
     Returns:
         dataframe: updated job application info
     """
-    global app_status
+    app_status = pd.DataFrame()
 
     st.toast("You Applied! Congrats")
-    new_app = [{'Company Name': data['company'], 'Position Title': data['title'], 'Location': data['location'], 'Status': 'Applied', 'Date' : datetime.now()}]
+    new_app = [{'Company Name': data['company'],
+                'Position Title': data['title'], 
+                'Location': data['location'], 
+                'Status': 'Applied', 
+                'Date' : datetime.now()}]
     app_status = pd.concat([app_status, pd.DataFrame(new_app)], ignore_index=True)
-    app_status = app_status.drop_duplicates(['Company Name', 'Position Title', 'Location', 'Status'])
-    app_status.to_csv(r'C:\Users\user\Desktop\GitHub\opentowork\app_status.csv', index = None, header=True) 
-
+    app_status = app_status.drop_duplicates(
+        ['Company Name', 'Position Title', 'Location', 'Status']
+        )
+    app_status.to_csv(
+        r'C:\Users\user\Desktop\GitHub\opentowork\app_status.csv', 
+        index = None, header=True
+        )
     return app_status
 
 def app(skills_resume, resume_content):
