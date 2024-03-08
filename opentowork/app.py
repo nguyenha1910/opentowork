82344d9db300a73882c6e990db95f173ae493d01
ymkim814 <78923564+ymkim814@users.noreply.github.com>
2024-03-07 19:27:42 -0800

    add dashboard default
    Add default status so that we can work with that even there is an empty status

--- opentowork/app.py
+++ opentowork/app.py
@@ -10,6 +10,7 @@ import yaml
 import streamlit as st
 from streamlit_tags import st_tags
 import skill_extraction
+import pandas as pd
 from scraper import job_listing_scraper
 # from opentowork import skill_extraction
 # from opentowork.pages import job_recommendation
@@ -18,6 +19,12 @@ from pages import job_recommendation
 with open("config.yml", "r", encoding='UTF-8') as config_file:
 
     config = yaml.safe_load(config_file)
+
+try:
+    status = pd.read_csv(r'C:\Users\user\Desktop\GitHub\opentowork\app_status.csv')
+except:
+    status = None #either dataframe or initiate col
+    pass
 
 for key, value in config.items():
     if isinstance(value, str):
