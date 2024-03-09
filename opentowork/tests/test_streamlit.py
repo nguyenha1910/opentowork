4557a2ca2cc22d387e5ea987a82f5d0a0b6e554c
Kelly <zwang28@uw.edu>
2024-03-09 09:21:12 -0800

    try 120

--- opentowork/tests/test_streamlit.py
+++ opentowork/tests/test_streamlit.py
@@ -46,7 +46,7 @@ class TestStreamlit(unittest.TestCase):
 
         mock_file_uploader.return_value = MockFileUploader()
 
-        self.at.run(timeout=60)
+        self.at.run(timeout=120)
 
     def test_title(self):
         """ Test title """
