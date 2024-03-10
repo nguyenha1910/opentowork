83ef18123a64e33ef13df5f973750e46952d59f1
Elaine Zhang <ezhang33@uw.edu>
2024-03-09 23:29:25 -0800

    consolidated linkedin_jobs and indeed_jobs as get_jobs

--- opentowork/scraper/get_jobs.py
+++ opentowork/scraper/get_jobs.py
@@ -1,3 +1,20 @@
+# pylint: disable=broad-exception-caught
+# disabled due to variability in scraping exceptions, no specific expected exception
+# pylint: disable=too-many-arguments
+# disabled due to all arguments necessary
+"""
+Module retrieves job information from LinkedIn and Indeed using Selenium and BeautifulSoup.
+Returns a list of job information stores as dictionaries.
+Functions:
+    clean_date - cleans scraped date for indeed job listings
+    calculate_pages - calculates the number of pages needed for specific number of jobs
+    get_url - creates url to scrape from
+    find_listings - uses BeautifulSoup to find job listings on search page
+    get_details - gathers job details from input generated with BeautifulSoup
+    get_description - retrieves job description from job link
+    scrape_listings - gathers job details for each job in search
+    scrape_search - initializes scraping and creates list of jobs
+"""
 from datetime import datetime
 import time
 import random
@@ -62,7 +79,53 @@ def calculate_pages(target_job_count):
     pages = math.ceil(target_job_count/job_listings_per_page)
     return (pages, job_listings_per_page)
 
+def get_url(source, job_title_input, start_index):
+    """
+    Function creates url to start scraping jobs from depending on job source.
+    Args:
+        source (string): either "LinkedIn" or "Indeed"
+        job_title_input (string): job title to search for
+        start_index (int): start index for search page, used to paginate
+    Returns:
+        url (string): search url to scrape from
+    """
+    if source == "LinkedIn":
+        base_url = "https://www.linkedin.com/jobs/search/?keywords="
+        url = base_url + f"{job_title_input}&location=UnitedStates&start={start_index}"
+    if source == "Indeed":
+        url = f"https://www.indeed.com/jobs?q={job_title_input}&l=United+States&start={start_index}"
+    return url
+
+def find_listings(soup, source):
+    """
+    Function takes BeautifulSoup input and finds job listings using specified class.
+    Args:
+        soup (BeautifulSoup): generated from scrape_search function
+        source (string): either "LinkedIn" or "Indeed"
+    Returns:
+        job_listings (ResultSet): contains div tags matching specified class
+    """
+    if source == "LinkedIn":
+        job_listings = soup.find_all("div", class_=("base-card relative w-full hover:no-underline "
+                                                        "focus:no-underline "
+                                                        "base-card--link base-search-card "
+                                                        "base-search-card--link job-search-card"))
+    if source == "Indeed":
+        job_listings = soup.find_all("div", class_=lambda x: x and
+                                    x.startswith('cardOutline tapItem dd-privacy-allow result job'))
+    return job_listings
+
 def get_details(job, source):
+    """
+    Function finds details of each job listing from job passed in from scrape_listings.
+    Uses BeautifulSoup to identify specific details.
+    Args:
+        job (Tag): contains details for a job in HTML
+        source (string): either "LinkedIn" or "Indeed"
+    Returns:
+        [job_title, job_company, job_location, posted_date, apply_link] (list):
+        list of job details
+    """
     if source == "LinkedIn":
         job_title = job.find("h3", class_="base-search-card__title").text.strip()
         job_company = job.find("h4", class_="base-search-card__subtitle").text.strip()
@@ -82,6 +145,14 @@ def get_details(job, source):
     return [job_title, job_company, job_location, posted_date, apply_link]
 
 def get_description(description_soup, source):
+    """
+    Function gets job description from job posting page using BeautifulSoup.
+    Args:
+        description_soup (BeautifulSoup): generated in scrape_listings function
+        source (string): either "LinkedIn" or "Indeed"
+    Returns:
+        job_description (string): job description retrieved from job posting
+    """
     if source == "LinkedIn":
         description = description_soup.find("div", class_=("description__text "
                                                             "description__text--"
@@ -92,26 +163,25 @@ def get_description(description_soup, source):
     job_description = description.strip().replace('\n', '')
     return job_description
 
-def get_url(source, job_title_input, start_index):
-    if source == "LinkedIn":
-        base_url = "https://www.linkedin.com/jobs/search/?keywords="
-        url = base_url + f"{job_title_input}&location=UnitedStates&start={start_index}"
-    if source == "Indeed":
-        url = f"https://www.indeed.com/jobs?q={job_title_input}&l=United+States&start={start_index}"
-    return url
-
-def find_listings(soup, source):
-    if source == "LinkedIn":
-        job_listings = soup.find_all("div", class_=("base-card relative w-full hover:no-underline "
-                                                        "focus:no-underline "
-                                                        "base-card--link base-search-card "
-                                                        "base-search-card--link job-search-card"))
-    if source == "Indeed":
-        job_listings = soup.find_all("div", class_=lambda x: x and
-                                    x.startswith('cardOutline tapItem dd-privacy-allow result job'))
-    return job_listings
-
 def scrape_listings(job_listings, driver, valid_job_count, last_page, target_job_count, source):
+    """
+    Function takes soup from BeautifulSoup and calls get_details, get_description
+    to generate job details. Iterates through each job in job_listings and stores
+    job details in a list of dictionaries.
+    Args:
+        job_listings (BeautifulSoup): generated from soup.find_all() looking for a
+        specific class
+        driver (Chrome webdriver): Chrome webdriver from selenium
+        valid_job_count (int): number of jobs with no None values
+        last_page (boolean): indicator for last page
+        target_job_count (int): targeted number of jobs to scrape
+        source (string): either "LinkedIn" or "Indeed"
+    Returns:
+    in a tuple:
+        listings (list): list of job info, each job is its own dictionary with the
+        job attribute as a key and scraped detail as value
+        valid_job_count (int): count of valid jobs scraped
+    """
     listings = []
     for job in job_listings:
         details = get_details(job, source)
@@ -142,6 +212,24 @@ def scrape_listings(job_listings, driver, valid_job_count, last_page, target_job
     return (listings, valid_job_count)
 
 def scrape_search(job_title_input, target_job_count, source):
+    """
+    Function initializes the scraping process by getting search url for the
+    inputed job title and number of pages. Calls calculate_pages, find_listings,
+    and scrape_listings functions. Returns one list with all the job listings.
+    Uses selenium and BeautifulSoup.
+    Args:
+        job_title_input (string): job title to search for
+        target_job_count (int): number of jobs to get
+        source (string): either "LinkedIn" or "Indeed"
+    Returns:
+        jobs (list): list of job details, each job is its own dictionary, generated
+        from scrape_indeed_listings
+    Exceptions:
+        TypeError for job_title_input (should be string)
+        TypeError for target_job_count (should be int)
+        TypeError for source (should be string)
+        ValueError for source (either "LinkedIn" or "Indeed")
+    """
     if isinstance(job_title_input, str) is not True:
         raise TypeError("Job title input is not a string")
     if isinstance(target_job_count, int) is not True:
@@ -177,7 +265,7 @@ def scrape_search(job_title_input, target_job_count, source):
         job_listings = find_listings(soup, source)
         try:
             results = scrape_listings(job_listings, driver, valid_job_count,
-                                      last_page, target_job_count)
+                                      last_page, target_job_count, source)
             # add data to the jobs list
             jobs = jobs + results[0]
             valid_job_count = results[1]
